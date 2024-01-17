681290a8732f16ccf40bacb856f1c76c6bf39b5a
supertz4r <juninsoader@gmail.com>
2024-01-17 18:46:05 -0300

    DTO

---
+++ src/main/java/com/devsuperior/movieflix/dto/MovieDTO.java
@@ -0,0 +1,37 @@
+package com.devsuperior.movieflix.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.movieflix.entities.Movie;
+
+public class MovieDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String title;
+	private String subTitle;
+	private Long year;
+	private String imgUrl;
+	private String synopsis;
+	
+	public MovieDTO(){}
+
+	public MovieDTO(Long id, String title, String subTitle, Long year, String imgUrl, String synopsis) {
+		this.id = id;
+		this.title = title;
+		this.subTitle = subTitle;
+		this.year = year;
+		this.imgUrl = imgUrl;
+		this.synopsis = synopsis;
+	}
+	
+	public MovieDTO(Movie entity) {
+		id = entity.getId();
+		title = entity.getTitle();
+		subTitle = entity.getSubTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		synopsis = entity.getSynopsis();
+	}
+
+}
