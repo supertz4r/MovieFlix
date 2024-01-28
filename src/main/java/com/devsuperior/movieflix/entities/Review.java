7617eebfbb54804ef3a3d7ae85e7452bc768e755
supertz4r <juninsoader@gmail.com>
2024-01-27 21:32:33 -0300

    Database seeding

--- src/main/java/com/devsuperior/movieflix/entities/Review.java
+++ src/main/java/com/devsuperior/movieflix/entities/Review.java
@@ -26,7 +26,7 @@ public class Review implements Serializable {
 	private User user;
 	
 	@ManyToOne
-	@JoinColumn(name = "move_id")
+	@JoinColumn(name = "movie_id")
 	private Movie movie;
 	
 	public Review(){}
